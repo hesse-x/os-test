1b6e7e0b18f9109467b00d94cc7e69904bde5af5
hesse <hx33212391@outlook.com>
2024-10-21 11:46:03 +0800

    fix: fix link error on gcc9

--- build_tools/macros.cmake
+++ build_tools/macros.cmake
@@ -1,12 +1,10 @@
 function(align_binary)
   set(options "")
-  set(oneValueArgs NAME SRC OUT)
+  set(oneValueArgs NAME SRC OUT PATH)
   cmake_parse_arguments(ALIGN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-  add_custom_command(
-      OUTPUT ${ALIGN_OUT}
-      COMMAND bash ${BUILD_TOOLS}/align.sh $<TARGET_FILE:${ALIGN_SRC}> ${ALIGN_OUT}
+  add_custom_target(${ALIGN_NAME}
+      bash ${BUILD_TOOLS}/align.sh ${ALIGN_PATH}/${ALIGN_SRC} ${ALIGN_OUT}
       DEPENDS ${ALIGN_SRC}
       COMMENT "Align load_kernel.bin"
   )
-  add_custom_target(${ALIGN_NAME} ALL DEPENDS ${ALIGN_OUT})
 endfunction()
